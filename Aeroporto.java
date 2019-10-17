5f973e5a56cf63c24d3921728f8b23df324364fd
Roselaine Moreira <49179422+Roselaine-Moreira@users.noreply.github.com>
2019-10-17 19:16:41 -0300

    Implementação do Aeroporto + toString inicial

--- Aeroporto.java
+++ Aeroporto.java
@@ -1,28 +1,30 @@
+package pucrs.myflight.modelo;
+
 public class Aeroporto {
-    private String codigo;
-    private String nome;
-    private Geo loc;
+	private String codigo;
+	private String nome;
+	private Geo loc;
+	
+	public Aeroporto(String codigo, String nome, Geo loc) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.loc = loc;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Geo getLocal() {
+		return loc;
+	}
 
-    public Aeroporto(String codigo, String nome, Geo loc) {
-        this.codigo = codigo;
-        this.nome = nome;
-        this.loc = loc;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public Geo getLocal() {
-        return loc;
-    }
-
-    @Override
-    public String toString(){
-        return nome + "("+codigo+")"+"\n";
-    }
+	@Override
+	public String toString() {
+		return codigo + " - " + nome + " [" + loc + "]";
+	}
 }
