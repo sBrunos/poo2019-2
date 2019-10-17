9dd08636edb7105898c826a1172f7c3675ab6104
Roselaine Moreira <49179422+Roselaine-Moreira@users.noreply.github.com>
2019-10-17 19:15:03 -0300

    Implementação da Aeronave + toString inicial

--- Aeronave.java
+++ Aeronave.java
@@ -1,21 +1,29 @@
+package pucrs.myflight.modelo;
+
 public class Aeronave {
-    private String codigo;
-    private String descricao;
+	private String codigo;
+	private String descricao;
+	private String capacidade;
+	
+	public Aeronave(String codigo, String descricao, String capacidade) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.capacidade = capacidade;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public Aeronave(String codigo, String descricao) {
-        this.codigo = codigo;
-        this.descricao = descricao;
-    }
+	public String getCapacidade() {
+		return capacidade;
+	}
 
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public String toString(){
-        return descricao + "("+codigo+")";
-    }
+	public String toString() {
+		return codigo + " - " + descricao + " (" + capacidade + ")";
+	}
 }
