300538dfbc6ca7a9a8f23b24b783a88962010660
Roselaine Moreira <49179422+Roselaine-Moreira@users.noreply.github.com>
2019-10-17 19:21:04 -0300

    Geo somente toString inicial

--- Geo.java
+++ Geo.java
@@ -1,23 +1,24 @@
+package pucrs.myflight.modelo;
+
 public class Geo {
-    private double latitude;
-    private double longitude;
+	private double latitude;
+	private double longitude;
+	
+	public Geo(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
 
-    public Geo(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-
-    @Override
-    public String toString(){
-        return "Latitude: " + latitude + " Longitude: "+ longitude + "\n";
-    }
+	@Override
+	public String toString() {
+		return super.toString()+ "Latitude"+ latitude + "longitude" + longitude;
+	}
 }
