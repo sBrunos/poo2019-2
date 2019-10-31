98de5e6488ccaeb22581993b39c8bf4d4be65018
Bruno Andrade <ba160497@gmail.com>
2019-10-31 11:11:51 -0300

    Criação do construtor com horário default

--- Voo.java
+++ Voo.java
@@ -2,6 +2,7 @@ package pucrs.myflight.modelo;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Month;
 
 public class Voo {
 	
@@ -17,7 +18,14 @@ public class Voo {
 		this.datahora = datahora;
 		this.duracao = duracao;
 		this.status = Status.CONFIRMADO; // default é confirmado
-	}
+    }
+    
+    public Voo(Rota rota, Duration duracao){
+        this.rota = rota;
+        this.duracao = duracao;
+        this.datahora = LocalDateTime.of(2016, Month.AUGUST, 12, 12, 00, 00);
+        this.status = Status.CONFIRMADO;
+    }
 	
 	public Rota getRota() {
 		return rota;
